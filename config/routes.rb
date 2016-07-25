f2514ebd71848221e3b2ef423b8c3b0f2ac2ac47
Johnathan Lyman <jelyman@gmail.com>
2016-07-25 08:33:23 -0700

    base edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
 
-  get 'pages/about'
+  get '/', to: 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+  get '/faq', to: 'pages#faq'
 
-  get 'pages/contact'
-
-  get 'pages/faq'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :app do
+    resources :users, as: :user
+  end
 end
