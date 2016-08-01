32372e1cb661afb02d57ae5489b18fe8c394feaf
Johnathan Lyman <jelyman@gmail.com>
2016-07-31 23:21:27 -0700

    more token handling work needed

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,14 +1,17 @@
 class ProfileController < ApplicationController
   before_filter :authenticate_user!
   def show
-
+    @hashkey = check_for_hashkey
   end
 
   def regenerate
     # Explicitly setting this to the logged in user keeps user A from changing user B's key
     id = current_user.id
-    @identity = Identity.find_by_user_id(id)
-    @identity.destroy!
+    begin
+      @identity = Identity.find_by_user_id(id).destroy
+    rescue
+      puts ''
+    end
 
     @identity = Identity.new
     @identity.hashkey = SecureRandom.hex(n=8)
@@ -16,5 +19,19 @@ class ProfileController < ApplicationController
     @identity.save
 
     redirect_to profile_path
+  end
+
+  def check_for_hashkey
+    begin
+      return current_user.identity.hashkey
+    rescue
+      return false
+    end
+  end
+
+  private
+
+  def create_hashkey
+
   end
 end
